dbbba3c54f92eff4e9ff80c55ee109bda4757e59
rahul-sequelstring <rahul.kushwah@sequelstring.com>
2024-12-07 17:11:20 +0530

    minor changes

--- app/pages/main/School/component/FaqSection/FaqSection.tsx
+++
@@ -1,9 +0,0 @@
-import Faq1 from "./component/Faq1"
-
-const FaqSection = ({ setContent, content, webColor, isEditable }: any) => {
-  return (
-    <Faq1 content={content} setContent={setContent} webColor={webColor} isEditable={isEditable}/>
-  )
-}
-
-export default FaqSection
--- app/pages/main/School/layout/common/constant.ts
+++
@@ -1,2 +0,0 @@
-export const smallHeaderHeight = '60px'
-export const largeHeaderHeight = '70px'
