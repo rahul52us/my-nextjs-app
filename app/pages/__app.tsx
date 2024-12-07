fc1ce6f60f7d2cb0dc6136d12460b64a35bc1c1a
rahul-sequelstring <rahul.kushwah@sequelstring.com>
2024-12-07 17:59:19 +0530

    minro changes

--- app/pages/__app.tsx
+++ app/pages/__app.tsx
@@ -1,15 +1,13 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
-import Layout from '../base64-tools/components/Layout';
+// import Layout from '../base64-tools/components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      {/* Wrap every page in the Layout */}
-      <Layout>
-        {/* Render the current page dynamically */}
+      {/* <Layout>
         <Component {...pageProps} />
-      </Layout>
+      </Layout> */}
     </ChakraProvider>
   );
 }
